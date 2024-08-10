73c0a37af04396318e94c66acfd6955ab541e65e
Antônio Vinícius <antoniovrdacosta@gmail.com>
2024-08-10 23:11:03 +0000

    feat: Add navbar responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
   const router = useRouter();
 
   const handlePurchaseClick = () => {
-    router.push('/checkout');
+    router.push("/checkout");
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
@@ -16,13 +22,37 @@ export default function Navbar() {
         <h1 className="text-lg md:text-2xl font-bold truncate">
           Congresso Louvor 2024
         </h1>
-        <div className="flex-shrink-0">
+        <div className="flex-shrink-0 md:hidden">
+          <button onClick={toggleMenu} className="focus:outline-none">
+            {menuOpen ? (
+              <XMarkIcon className="h-6 w-6 text-white" />
+            ) : (
+              <Bars3Icon className="h-6 w-6 text-white" />
+            )}
+          </button>
+        </div>
+        <div className={`hidden md:flex md:items-center`}>
           <button
             onClick={handlePurchaseClick}
             className="bg-orange-500 hover:bg-orange-600 text-xs md:text-sm lg:text-base font-bold py-2 px-3 lg:px-4 rounded whitespace-nowrap"
           >
             Quero comprar meu ingresso
           </button>
+        </div>
+      </div>
+      <div
+        className={`overflow-hidden transition-[max-height] duration-500 ease-in-out md:hidden ${
+          menuOpen ? "max-h-40" : "max-h-0"
+        }`}
+      >
+        <div className="flex flex-col items-start space-y-2">
+          <button
+            onClick={handlePurchaseClick}
+            className="w-full mt-2 text-center bg-orange-500 hover:bg-orange-600 text-xs md:text-sm lg:text-base font-bold py-2 px-3 lg:px-4 rounded whitespace-nowrap"
+          >
+            Quero comprar meu ingresso
+          </button>
+          {/* Adicione outros itens de menu aqui se houver */}
         </div>
       </div>
     </nav>
