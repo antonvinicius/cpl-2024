947b9ff43b45176cf1cc5466ee60629c311fd318
Antônio Vinícius <antoniovrdacosta@gmail.com>
2024-08-12 23:16:50 +0000

    feat: Improve login page

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -8,12 +8,13 @@ import {
   EyeIcon,
   EyeSlashIcon,
 } from "@heroicons/react/24/solid";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter();
 
   const handleLogin = (e) => {
@@ -22,7 +23,9 @@ export default function LoginPage() {
       localStorage.setItem("user", "admin");
       router.push("/admin"); // Redireciona para a página de administração
     } else {
-      setErrorMessage("Invalid username or password");
+      toast.error("Usuário ou senha inválidos", {
+        position: "top-center",
+      });
     }
   };
 
@@ -33,16 +36,21 @@ export default function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-800 via-gray-900 to-black">
       <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
-        <div className="text-center mb-6">
-          <h1 className="text-3xl font-bold text-white">Welcome Back</h1>
-          <p className="text-gray-400">Please login to your account</p>
+        <div className="flex flex-col items-center text-center mb-6">
+          <img
+            className="text-center"
+            src="/autorizacao.png"
+            alt="Admin Page"
+            width={200}
+          />
+          <p className="text-gray-400">Área administrativa - Faça o login</p>
         </div>
         <form onSubmit={handleLogin} className="space-y-4">
           <div className="relative">
             <UserIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
             <input
               type="text"
-              placeholder="Username"
+              placeholder="Usuário"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               className="w-full pl-10 pr-4 py-2 rounded bg-gray-900 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -52,7 +60,7 @@ export default function LoginPage() {
             <LockClosedIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
             <input
               type={showPassword ? "text" : "password"}
-              placeholder="Password"
+              placeholder="Senha"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="w-full pl-10 pr-10 py-2 rounded bg-gray-900 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -69,19 +77,15 @@ export default function LoginPage() {
               )}
             </button>
           </div>
-          {errorMessage && (
-            <div className="text-red-500 text-sm text-center">
-              {errorMessage}
-            </div>
-          )}
           <button
             type="submit"
             className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded font-bold"
           >
-            Login
+            Entrar
           </button>
         </form>
       </div>
+      <ToastContainer />
     </div>
   );
 }
